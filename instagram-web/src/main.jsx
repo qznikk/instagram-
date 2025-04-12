18423c6da255711de34c6d73ac9ed6a0b881dd00
qznikk <115718297+qznikk@users.noreply.github.com>
2025-04-12 17:28:24 +0200

    wyglad

--- instagram-web/src/main.jsx
+++ instagram-web/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/main.css";
+import "./styles/nav.css";
+import "./styles/styles.css";
+import "./styles/auth.css";
+import "./styles/profile.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
